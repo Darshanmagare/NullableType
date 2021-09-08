4a9e1b661e3e26cb72e06d8a38c505d42983124c
darshan magare <darshan.magare1234@gmail.com>
2021-09-08 11:02:38 +0530

    Add project files.

---
+++ NullableType/Nullable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace NullableType
+{
+    class Nullable
+    {
+        static void Main(string[] args)
+        {
+            // defining Nullable type
+            Nullable<int> n = null;
+
+            // using the method
+            // output will be 0 as default
+            // value of null is 0
+            Console.WriteLine(n.GetValueOrDefault());
+
+            // defining Nullable type
+            int? n1 = null;
+
+            // using the method
+            // output will be 0 as default
+            // value of null is 0
+            Console.WriteLine(n1.GetValueOrDefault());
+
+
+            // using Nullable type syntax
+            // to define non-nullable
+            int? n2 = 47;
+
+            // using the method
+            Console.WriteLine(n2.GetValueOrDefault());
+
+            // using Nullable type syntax
+            // to define non-nullable
+            Nullable<int> n3 = 457;
+
+            // using the method
+            Console.WriteLine(n3.GetValueOrDefault());
+
+        }
+
+    
+
+    }
+}
